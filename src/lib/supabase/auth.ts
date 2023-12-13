f2a847b9af367f3e24c60db89138baecd99b4614
tudorborca <borcatudor@gmail.com>
2023-12-13 15:16:01 +0200

    create app layout and add util functions

--- src/lib/supabase/auth.ts
+++ src/lib/supabase/auth.ts
@@ -1,4 +1,6 @@
+import useAuth from "~/store/useAuth";
 import { supabase } from "./client";
+import { AuthChangeEvent, Session } from "@supabase/supabase-js";
 
 export const signInWithGoogle = async () => {
 	return await supabase.auth.signInWithOAuth({
@@ -16,4 +18,22 @@ export const signInWithGithub = async () => {
 	return await supabase.auth.signInWithOAuth({
 		provider: "github",
 	});
+};
+
+const handlerFuncs: Record<AuthChangeEvent, () => void> = {
+	INITIAL_SESSION: () => {},
+	SIGNED_IN: () => {},
+	SIGNED_OUT: () => {},
+	TOKEN_REFRESHED: () => {},
+	USER_UPDATED: () => {},
+	MFA_CHALLENGE_VERIFIED: () => {},
+	PASSWORD_RECOVERY: () => {},
+};
+
+export const onAuthStateChangedHandler = () => {
+	const [context] = useAuth();
+
+	return async (event: AuthChangeEvent, session: Session) => {
+		handlerFuncs?.[event]();
+	};
 };
--- src/pages/chat/layout.tsx
+++ src/pages/chat/layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from "@solidjs/router";
+import Header from "~/components/header";
+import Sidebar from "~/components/sidebar";
+
+import { Flex } from "~/components/ui/flex";
 
 export default function ChatLayout() {
 	return (
-		<section>
-			<h1>Chat Layout</h1>
-			<Outlet />
-		</section>
+		<Flex flexDirection="row" class="w-screen h-screen">
+			<Sidebar />
+			<section class="w-full h-screen">
+				<Header />
+				<Outlet />
+			</section>
+		</Flex>
 	);
 }
