27260cf0f35a17af953953796274fd2cf26459c2
tudorborca <borcatudor@gmail.com>
2023-12-13 15:13:35 +0200

    work on auth and onboarding flow

--- src/components/protected.tsx
+++ src/components/protected.tsx
@@ -1,27 +1,52 @@
-import { Navigate, useMatch } from "@solidjs/router";
+import { Navigate } from "@solidjs/router";
 import { Match, ParentComponent, Show, Switch } from "solid-js";
-import useSession from "~/store/useSession";
+import { matchRoute } from "~/lib/utils";
+import useAuth from "~/store/useAuth";
+
+const WithOnboarding: ParentComponent = (props) => {
+	const [ctx] = useAuth();
+	const isOnboardingRoute = matchRoute("/onboarding");
+
+	return (
+		<Switch fallback={props.children}>
+			<Match
+				when={isOnboardingRoute && !ctx.user()?.user_metadata.onboarded}
+			>
+				{props.children}
+			</Match>
+			<Match
+				when={
+					!isOnboardingRoute && !ctx.user()?.user_metadata.onboarded
+				}
+			>
+				<Navigate href="/onboarding" />
+			</Match>
+			<Match
+				when={isOnboardingRoute && ctx.user()?.user_metadata.onboarded}
+			>
+				<Navigate href="/profile" />
+			</Match>
+		</Switch>
+	);
+};
 
 const Protected: ParentComponent = (props) => {
-	const [ctx] = useSession();
-	const match = (checkedPath: string) => useMatch(() => checkedPath)();
+	const [ctx] = useAuth();
+	const isAuthRoute = matchRoute("/auth");
 
 	return (
 		<Show when={!ctx.loading()} fallback={<h1>Loading...</h1>}>
-			<Switch>
-				<Match when={match("/auth") && ctx.session()}>
+			<Switch
+				fallback={<WithOnboarding>{props.children}</WithOnboarding>}
+			>
+				<Match when={isAuthRoute && !ctx.session()}>
+					{props.children}
+				</Match>
+				<Match when={isAuthRoute && ctx.session()}>
 					<Navigate href="/profile" />
 				</Match>
-				<Match when={!match("/auth") && !ctx.session()}>
+				<Match when={!isAuthRoute && !ctx.session()}>
 					<Navigate href="/auth" />
-				</Match>
-				<Match
-					when={
-						(match("/auth") && !ctx.session()) ||
-						(!match("/auth") && ctx.session())
-					}
-				>
-					{props.children}
 				</Match>
 			</Switch>
 		</Show>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,11 @@
-import type { ClassValue } from "clsx"
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { useMatch } from "@solidjs/router";
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const matchRoute = (checkedPath: string) =>
+	useMatch(() => checkedPath)();
